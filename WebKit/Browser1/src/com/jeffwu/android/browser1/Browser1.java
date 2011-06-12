410ccb5c96f1d83e5eb4cf1b1d40a2be9cdd946e
jeffwu <jeffwu_cn@hotmail.com>
2011-06-12 18:19:03 +0800

    init project with eclipse

---
+++ WebKit/Browser1/src/com/jeffwu/android/browser1/Browser1.java
@@ -0,0 +1,13 @@
+package com.jeffwu.android.browser1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Browser1 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
