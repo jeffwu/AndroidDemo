c9995d97c3a3b47ceb9f2b75fb4121cf22f5f4d9
jeffwu <jeffwu_cn@hotmail.com>
2011-06-12 20:49:39 +0800

    browser complate

--- WebKit/Browser1/src/com/jeffwu/android/browser1/Browser1.java
+++ WebKit/Browser1/src/com/jeffwu/android/browser1/Browser1.java
@@ -2,12 +2,26 @@ package com.jeffwu.android.browser1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Browser1 extends Activity {
+	WebView browser;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        browser = (WebView)findViewById(R.id.webkit);
+
+        // javascript 默认不被启动，这里进行启用
+        browser.getSettings().setJavaScriptEnabled(true);
+   
+        // 直接调用 URL 网址，是方式之一
+        //browser.loadUrl("http://www.fresh3g.net/blog/");
+        
+        // 加载数据，是方式之二
+        browser.loadData("<html><head><title>html show</title></head><body>html show</body></html>", "text/html", "utf-8");
     }
 }
